b0b0868f1c19d2f803808b9fcfa914c98eaf0470
“zhoufangbing” <1978467950@qq.com>
2018-12-03 17:41:44 +0800

    demo

---
+++ src/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.less';
+class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <span>1</span>
+      </div>
+    );
+  }
+}
+
+export default Header;
